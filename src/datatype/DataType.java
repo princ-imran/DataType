ec8715cea37df7fa86fba9a5e5a684c67ed19567
Imran Hoshain <princ.imran@gmail.com>
2020-02-12 10:01:28 +0600

    Java Data Type and Veriable

---
+++ src/datatype/DataType.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datatype;
+
+/**
+ *
+ * @author Spy Hacker
+ */
+public class DataType {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        char b = 'a';
+        short c =32677;
+        int d =126587;
+        float e = 10.2f;
+        double f =10.00;
+        
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+    
+}
